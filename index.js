513eb9b148bbc797ea534a3167a566721e5b6b3b
mccoysc <364589886@qq.com>
2023-05-13 00:55:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mqtt = require("mqtt");
 const http = require("http");
 const crypto = require("crypto");
-const Buffer=require("Buffer");
 
 
 const replyTimeout = process.env.REPLY_TIMEOUT || 30000;
